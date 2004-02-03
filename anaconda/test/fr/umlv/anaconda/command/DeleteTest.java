43fed80e48bc775c453cbc9758168d07dc1844ea
ofiguero <ofiguero>
2004-02-03 17:25:34 +0000

    *** empty log message ***

---
+++ anaconda/test/fr/umlv/anaconda/command/DeleteTest.java
@@ -0,0 +1,32 @@
+/*
+ * Créé le 3 févr. 2004
+ *
+ * Pour changer le modèle de ce fichier généré, allez à :
+ * Fenêtre&gt;Préférences&gt;Java&gt;Génération de code&gt;Code et commentaires
+ */
+package fr.umlv.anaconda.command;
+
+import junit.framework.TestCase;
+import java.io.File;
+/**
+ * @author ofiguero
+ *
+ * Pour changer le modèle de ce commentaire de type généré, allez à :
+ * Fenêtre&gt;Préférences&gt;Java&gt;Génération de code&gt;Code et commentaires
+ */
+public class DeleteTest extends TestCase {
+
+	/**
+	 * Constructor for DeleteTest.
+	 * @param arg0
+	 */
+	public DeleteTest(String arg0) {
+		super(arg0);
+	}
+	
+	public void testDelete(){
+		File file = new File("/home/main01/ofiguero/tmp/coucou/");
+		Delete delete = new Delete();
+		delete.run(file);
+	}
+}
